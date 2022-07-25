d7c3665757c35431deb49d020e1d950b8285f6b8
Juliao96 <julio.brito@allin.com.br>
2022-07-25 12:29:36 -0300

    add types on styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: #8257e6;
+  font-size: 64px;
+`
 
 export function App() {
   return (
     <div className="App">
-      <h1> Hello World </h1>
+      <Title> Hello World </Title>
     </div>
   );
 }
