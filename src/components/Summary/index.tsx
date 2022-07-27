c8eaaf016175ef11d42e58b2b7d91fa74afe8a01
Juliao96 <julio.brito@allin.com.br>
2022-07-27 17:02:46 -0300

    add Dashboard and Summary component

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,41 @@
+import incomeIMG from '../../assets/income.svg';
+import outcomeIMG from '../../assets/outcome.svg';
+import totalIMG from '../../assets/total.svg';
+
+import { Container } from "./styles";
+
+export function Summary() {
+    return (
+        <>
+            <Container>
+                <div>
+                    <header>
+                        <p>Entradas</p>
+                        <img src={incomeIMG} alt="Entradas" />
+                    </header>
+                    <strong>
+                        + R$1000,00
+                    </strong>
+                </div>
+                <div>
+                    <header>
+                        <p>Saídas</p>
+                        <img src={outcomeIMG} alt="Saídas" />
+                    </header>
+                    <strong>
+                        - R$500,00
+                    </strong>
+                </div>
+                <div className="highlight-background">
+                    <header>
+                        <p>Total</p>
+                        <img src={totalIMG} alt="Total" />
+                    </header>
+                    <strong>
+                        R$500,00
+                    </strong>
+                </div>
+            </Container>
+        </>
+    )
+}
