fa5ee1e97e9457e8cdd0b1cfa19ebade1c8e0e11
Juliao96 <julio.brito@allin.com.br>
2022-08-01 18:13:34 -0300

    add miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () =>{
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amout: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date(),
+        }
+      ]
+    })
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
