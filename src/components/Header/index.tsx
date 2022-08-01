6a9da6a50a68122883e1dc86eecd6da5d8259a87
Juliao96 <julio.brito@allin.com.br>
2022-08-01 18:51:01 -0300

    add and configure react-modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,18 @@
+import { useState } from 'react';
 import logoIMG from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
 
-export function Header () {
+export function Header ({ onOpenNewTransactionModal }: HeaderProps) {
     return (
         <>
             <Container>
                 <Content>
                 <img src={logoIMG} alt="dt money" />
-                <button type="button"> 
+                <button type="button" onClick={onOpenNewTransactionModal}> 
                     Nova transação 
                 </button>
                 </Content>
